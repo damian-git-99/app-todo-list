a0fe3111ca8103dd44b679f7e3e421deabe53906
damian <damiangalvan66@gmail.com>
2022-10-08 08:40:22 -0500

    add springboot project

---
+++ backend/src/main/java/com/github/damian_git_99/backend/AppTodoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.damian_git_99.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTodoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppTodoBackendApplication.class, args);
+	}
+
+}
