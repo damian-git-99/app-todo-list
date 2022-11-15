16a875c706f4aad3154fd5e4f57540fb4fdc5fbd
damian <damiangalvan66@gmail.com>
2022-11-15 11:59:06 -0600

    minor changes

--- backend/src/main/java/com/github/damian_git_99/backend/exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/com/github/damian_git_99/backend/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.github.damian_git_99.backend.exceptions;
 
-import com.github.damian_git_99.backend.user.exceptions.EmailAlreadyTakenException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,7 +8,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-public class GlobalHandlerException {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(InternalServerException.class)
     ResponseEntity<Map<String, Object>> handleInternalError(InternalServerException exception){
