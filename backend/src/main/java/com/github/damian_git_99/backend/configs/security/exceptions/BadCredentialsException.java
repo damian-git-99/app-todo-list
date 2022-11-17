23eadeff391624d1f067c77fd4667c7fd8731ce7
damian <damiangalvan66@gmail.com>
2022-11-17 16:58:46 -0600

    delete unused code

--- backend/src/main/java/com/github/damian_git_99/backend/configs/security/exceptions/BadCredentialsException.java
+++
@@ -1,7 +0,0 @@
-package com.github.damian_git_99.backend.configs.security.exceptions;
-
-public class BadCredentialsException extends RuntimeException {
-    public BadCredentialsException(String message) {
-        super(message);
-    }
-}
