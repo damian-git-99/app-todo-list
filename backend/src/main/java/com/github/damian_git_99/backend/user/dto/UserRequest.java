f766c6353c4420db778e2cad7e848e49de6bd0f2
damian <damiangalvan66@gmail.com>
2022-11-24 09:56:31 -0600

    minor changes

--- backend/src/main/java/com/github/damian_git_99/backend/user/dto/UserRequest.java
+++ backend/src/main/java/com/github/damian_git_99/backend/user/dto/UserRequest.java
@@ -1,11 +1,15 @@
 package com.github.damian_git_99.backend.user.dto;
 
+import com.github.damian_git_99.backend.user.models.User;
 import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
+/**
+ * A DTO for the {@link User} entity
+ */
 @Builder
 @Data
 public class UserRequest {
