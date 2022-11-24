e666bf47ed214f9004d51f9724cf7fb791d6b58e
damian <damiangalvan66@gmail.com>
2022-11-24 09:57:19 -0600

    minor changes

--- backend/src/main/java/com/github/damian_git_99/backend/project/services/ProjectService.java
+++ backend/src/main/java/com/github/damian_git_99/backend/project/services/ProjectService.java
@@ -1,5 +1,6 @@
 package com.github.damian_git_99.backend.project.services;
 
+import com.github.damian_git_99.backend.project.exceptions.ForbiddenProjectException;
 import com.github.damian_git_99.backend.project.models.Project;
 import com.github.damian_git_99.backend.project.dto.ProjectRequest;
 import com.github.damian_git_99.backend.configs.security.AuthenticatedUser;
@@ -7,7 +8,7 @@ import com.github.damian_git_99.backend.configs.security.AuthenticatedUser;
 public interface ProjectService {
 
     void createProject(ProjectRequest projectRequest, Long userId);
-    Project findProjectById(Long projectId, AuthenticatedUser authenticatedUser);
-    void deleteProjectById(Long projectId, AuthenticatedUser authenticatedUser);
+    Project findProjectById(Long projectId, AuthenticatedUser authenticatedUser) throws ForbiddenProjectException;
+    void deleteProjectById(Long projectId, AuthenticatedUser authenticatedUser) throws ForbiddenProjectException;
 
 }
