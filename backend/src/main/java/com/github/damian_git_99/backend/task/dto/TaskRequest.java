9c33e8cdc9fc00a2c97a920e2da7cac100661822
damian <damiangalvan66@gmail.com>
2022-11-27 18:27:50 -0600

    fix error

--- backend/src/main/java/com/github/damian_git_99/backend/task/dto/TaskRequest.java
+++ backend/src/main/java/com/github/damian_git_99/backend/task/dto/TaskRequest.java
@@ -3,14 +3,17 @@ package com.github.damian_git_99.backend.task.dto;
 import com.github.damian_git_99.backend.task.models.Priority;
 import com.github.damian_git_99.backend.task.models.Task;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 /**
  * A DTO for the {@link Task} entity
  */
+@Builder
 @AllArgsConstructor
 @Getter
 public class TaskRequest implements Serializable {
@@ -18,6 +21,6 @@ public class TaskRequest implements Serializable {
     private final String taskName;
     @NotEmpty
     private final String description;
-    @NotEmpty
+    @NotNull
     private final Priority priority;
 }
