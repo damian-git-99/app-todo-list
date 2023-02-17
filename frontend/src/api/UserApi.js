b8eb62ad010a0186163ce56df9a9e9d7f974dac8
damian <damiangalvan66@gmail.com>
2023-02-17 12:31:05 -0600

    Update UserApi to use URL constant for API requests

--- frontend/src/api/UserApi.js
+++ frontend/src/api/UserApi.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const URL = 'http://127.0.0.1:8080/api/v1';
+
 export const signup = async (user) => {
   try {
-    const { data } = await axios.post('http://127.0.0.1:8080/api/v1/users', user);
+    const { data } = await axios.post(`${URL}/users`, user);
     return data;
   } catch (error) {
     const message = error?.response?.data?.error || error.message;
@@ -13,7 +15,7 @@ export const signup = async (user) => {
 
 export const login = async (user) => {
   try {
-    const { data } = await axios.post('http://127.0.0.1:8080/api/v1/auth', user);
+    const { data } = await axios.post(`${URL}/auth`, user);
     const { username, email } = await userInfo(data.token);
     localStorage.setItem('token', JSON.stringify(data.token));
     return {
@@ -36,7 +38,7 @@ export const userInfo = async (token) => {
         Authorization: `Bearer ${token}`
       }
     };
-    const { data } = await axios.get('http://127.0.0.1:8080/api/v1/users/info', config);
+    const { data } = await axios.get(`${URL}/users/info`, config);
     return data;
   } catch (error) {
     const status = error.response.status;
@@ -57,7 +59,7 @@ export const updateUser = async (id, user, token) => {
         Authorization: `Bearer ${token}`
       }
     };
-    const { data } = await axios.put(`http://127.0.0.1:8080/api/v1/users/${id}`, user, config);
+    const { data } = await axios.put(`${URL}/users/${id}`, user, config);
     return data;
   } catch (error) {
     const message = error?.response?.data?.error || error.message;
