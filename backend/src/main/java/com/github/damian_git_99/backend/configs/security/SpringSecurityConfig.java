b7b3ccf32f3934220bfac7307fd1e4942b2e9c31
damian <damiangalvan66@gmail.com>
2023-02-17 14:10:11 -0600

    Update CorsConfigurationSource to allow requests from localhost and Vite-frontend.

--- backend/src/main/java/com/github/damian_git_99/backend/configs/security/SpringSecurityConfig.java
+++ backend/src/main/java/com/github/damian_git_99/backend/configs/security/SpringSecurityConfig.java
@@ -77,7 +77,7 @@ public class SpringSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://127.0.0.1:5173"));
+        config.setAllowedOrigins(List.of("http://vite-frontend:5173","http://localhost:5173"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
 
